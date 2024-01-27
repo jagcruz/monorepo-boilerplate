f07282b68e8d03f465990262120b80ff22834056
Jaime Alan Gutiérrez Cruz <jgutierrezcruz367@gmail.com>
2024-01-27 04:49:25 -0300

    feat: add Turborepo

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { isBlank, get } from '@jagc/package-common';
+import { isBlank, get } from '@jagcruz/package-common';
 
 // eslint-disable-next-line import/no-unresolved
 import viteLogo from '/vite.svg';
